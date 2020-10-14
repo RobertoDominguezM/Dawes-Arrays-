8b6a8914caf0941dbd74fc79bf6f50bb99a8157f
Roberto Domínguez <201902@CALASANZSALAMANCA.LOCAL>
2020-10-14 12:56:22 +0200

    ejFruteria

--- PracticaFruta/Script/fruteria.js
+++ PracticaFruta/Script/fruteria.js
@@ -1,4 +1,13 @@
-var kilos=0;
-function sumaKilos(){
-    kilos = kilos+1;
+var cantidad = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var kilos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var frutas = ['sandia', 'naranja', 'melocotones', 'mandarina', 'manzana', 'platano', 'cerezas', 'kiwi', 'fresa', 'piña'];
+function anadir(frutas1){
+    var i;
+    for(i = 0; i < frutas.length;i++){
+        if(frutas[i] == frutas1){
+            cantidad[i]+=1;
+            kilos[i]+=1;
+            break;
+        }
+    }
 }
